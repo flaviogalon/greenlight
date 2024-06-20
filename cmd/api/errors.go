67c397dca146db23f1bc5bce3ed6c12aeaeab9c3
Flavio Galon <flaviogalon@gmail.com>
2024-06-20 17:56:49 +0100

    Protect update movie handler with optimistic locking
    Use 'version' field when updating a record. Only update a record when
    the 'version' matches the one previously fetched from DB.

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -51,10 +51,16 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
+// Helper method to be used to send a 422 to the client
 func (app *application) failedValidationResponse(
 	w http.ResponseWriter,
 	r *http.Request,
 	errors map[string]string,
 ) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
 }
